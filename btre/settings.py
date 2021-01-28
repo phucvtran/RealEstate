23b75926f659a948d044c2cfbdd3e98e241bc0b0
Rim <Rim@DESKTOP-CI0N2AL>
2021-01-27 17:59:41 -0800

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'tran@pullova.com'
 EMAIL_HOST_PASSWORD = 'Seattle2018'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
